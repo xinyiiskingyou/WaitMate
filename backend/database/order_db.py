203e4d3f5e8e4d7670caf1e7cef336218be18ffa
xinyiiskingyou <xinyi_lin217@hotmail.com>
2023-06-20 23:44:50 +1000

    added functions for history order

--- backend/database/order_db.py
+++ backend/database/order_db.py
@@ -1,14 +1,14 @@
 import sqlite3
 import datetime
 
-class Orders_db:
+class OrderDB:
     ORDER_DB_PATH = "./database/order.db"
 
-    def __init__(self, db=ORDER_DB_PATH):
-        self.db = db
+    def __init__(self, database=ORDER_DB_PATH):
+        self.database = database
 
     def create_order_table(self):
-        con = sqlite3.connect(self.db)
+        con = sqlite3.connect(self.database)
         cur = con.cursor()
 
         cur.execute("PRAGMA foreign_keys = OFF")
@@ -25,7 +25,7 @@ class Orders_db:
         con.close()
 
     def add_order(self, table_id, item_name, amount):
-        con = sqlite3.connect(self.db)
+        con = sqlite3.connect(self.database)
         cur = con.cursor()
         curr_time = datetime.datetime.now()
         timestamp = curr_time.strftime("%H:%M:%S")
@@ -36,7 +36,7 @@ class Orders_db:
         con.close()
 
     def table_order_list(self, table_id):
-        con = sqlite3.connect(self.db)
+        con = sqlite3.connect(self.database)
         cur = con.cursor()
 
         cur.execute("SELECT item_name, amount FROM Orders WHERE table_id = ?", (table_id,))
@@ -46,7 +46,7 @@ class Orders_db:
         return order_list
 
     def get_all_orders(self):
-        con = sqlite3.connect(self.db)
+        con = sqlite3.connect(self.database)
         cur = con.cursor()
 
         cur.execute("SELECT * FROM Orders ORDER BY timestamp ASC")
@@ -54,3 +54,43 @@ class Orders_db:
 
         con.close()
         return order_list
+
+class HistoryOrder:
+
+    HISTORY_DB_PATH = "./database/history.db"
+
+    def __init__(self, database=HISTORY_DB_PATH) -> None:
+        self.database = database
+
+    def create_history_table(self):
+        con = sqlite3.connect(self.database)
+        cur = con.cursor()
+
+        cur.execute("PRAGMA foreign_keys = OFF")
+        con.commit()
+        cur.execute('''CREATE TABLE IF NOT EXISTS HistoryOrder (
+                        customer_id INTEGER NOT NULL,
+                        item_name TEXT NOT NULL
+                    )''')
+        con.commit()
+        con.close()
+
+    def add_to_history(self, customer_id, item_name):
+        con = sqlite3.connect(self.database)
+        cur = con.cursor()
+
+        cur.execute("INSERT INTO HistoryOrder (customer_id, item_name) \
+        VALUES (?, ?)", (customer_id, item_name))
+        con.commit()
+
+        con.close()
+
+    def get_history(self, customer_id):
+        con = sqlite3.connect(self.database)
+        cur = con.cursor()
+
+        cur.execute("SELECT item_name FROM HistoryOrder WHERE customer_id = ?", (customer_id,))
+        history_order_list = cur.fetchall()
+
+        con.close()
+        return history_order_list
