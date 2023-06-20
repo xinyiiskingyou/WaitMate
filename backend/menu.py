f653b2b3504dcaedf83701ee8146b09d43d290f9
Trinh <trinh.pham2277@gmail.com>
2023-06-20 14:45:40 +1000

    added functionality for db of adding menus/categories

---
+++ backend/menu.py
@@ -0,0 +1,114 @@
+import sqlite3
+
+def category_already(name: str) -> bool:
+    con = sqlite3.connect("restaurant.db")
+    cur = con.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS categories(name)")
+    cur.execute("SELECT 1 FROM categories c WHERE c.name = (?)",(name,))
+    items = cur.fetchall()
+    con.commit()
+    con.close()
+    
+    if len(items) == 1:
+        return True
+    return False
+
+def item_already(name: str) -> bool:
+    con = sqlite3.connect("restaurant.db")
+    cur = con.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS items(name, cost, description)")
+    cur.execute("SELECT 1 FROM items i WHERE i.name = (?)",(name,))
+    items = cur.fetchall()
+    con.commit()
+    con.close()
+    
+    if len(items) == 1:
+        return True
+    return False
+
+def get_next_order(category) -> int:
+    con = sqlite3.connect("restaurant.db")
+    cur = con.cursor()
+    cur.execute("SELECT * FROM menu m WHERE m.category = (?)",(category,))
+    items = cur.fetchall()
+    con.commit()
+    con.close()    
+    return len(items)
+
+def category_add(name: str) -> None:
+    if len(name) < 1 or len(name) > 15:
+        return
+    if category_already(name):
+        print("exist")
+        return
+    con = sqlite3.connect("restaurant.db")
+    cur = con.cursor()
+    cur.execute("INSERT INTO categories values(?)",(name,))
+    con.commit()
+    con.close()
+
+
+def item_add(category: str, name: str, cost: float, description: str) -> None:
+    if len(name) < 1 or len(name) > 15:
+        return
+    if item_already(name):
+        print("exist")
+        return
+    con = sqlite3.connect("restaurant.db")
+    cur = con.cursor()
+    cur.execute("INSERT INTO items values(?,?,?)",(name,cost,description))
+    cur.execute("CREATE TABLE IF NOT EXISTS menu(category, item, order)")
+    order: int = get_next_order(category)
+    cur.execute("INSERT INTO menu values(?,?)",(category,name, order))
+    con.commit()
+    con.close()
+
+def menu_view() -> dict[str, list[dict]]:
+    menu: dict[str, list[dict]]
+    con = sqlite3.connect("restaurant.db")
+    cur = con.cursor()
+    cur.execute("SELECT * FROM menu INNER JOIN items ON menu.item = items.name")
+    items = cur.fetchall()
+    for item in items:
+        print(item)
+    con.commit()
+    con.close()  
+    return menu  
+
+def sql_show_all() -> None:
+    con = sqlite3.connect("restaurant.db")
+    cur = con.cursor()
+    print("Categories")
+    cur.execute("SELECT rowid, * FROM categories")
+    items = cur.fetchall()
+    for item in items:
+        print(item)
+
+    print("Items")
+    cur.execute("SELECT rowid, * FROM items")
+    items = cur.fetchall()
+    for item in items:
+        print(item)   
+
+    print("Menu")
+    cur.execute("SELECT rowid, * FROM menu")
+    items = cur.fetchall()
+    for item in items:
+        print(item)  
+
+    con.commit()
+    con.close()
+
+if __name__ == "__main__":
+    con = sqlite3.connect("restaurant.db")
+    cur = con.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS categories (name)")
+    con.commit()
+    con.close()
+    category_add("Yms")
+    item_add("Yms", )
+
+    sql_show_all()
+
+
+
--- backend/server.py
+++ backend/server.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
-
+from menu import category_add, item_add, menu_view
 app = FastAPI()
 
 @app.get("/menu/category/add")
-def category_add_api(name: str):
+def category_add_api(name: str):    
+    category_add(name)
     return {}
+
+@app.get("/menu/item/add")
+def item_add_api(category: str, name: str, cost: float, description: str):    
+    item_add(category, name, cost, description)
+    return {}
+
+
+@app.get("/menu/listall")
+def menu_view_api():
+    return menu_view()
