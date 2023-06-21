91df7c6f63550536c51388bc0aef0a979ac94b3a
Trinh <trinh.pham2277@gmail.com>
2023-06-21 12:50:57 +1000

    Deletion from moving file to directory

--- backend/app/api.py
+++ backend/app/api.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import sys
 sys.path.append('..')
-from menu import (
+from backend.src.menu import (
     category_add, item_add, menu_view, menu_item_update_details, menu_category_update_details,
     menu_item_remove, menu_item_update_order, menu_category_update_order
 )
