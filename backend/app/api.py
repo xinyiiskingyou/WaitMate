b27396db1ec98aee9ff31d552104a30f327dbb36
xinyiiskingyou <xinyi_lin217@hotmail.com>
2023-06-21 19:19:55 +1000

    added simple api methods for order

--- backend/app/api.py
+++ backend/app/api.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from src.order_db import OrderDB
 from src.table_db import TableDB
 
 app = FastAPI()
+order = OrderDB()
 table = TableDB()
 
 origins = [
@@ -24,9 +26,26 @@ app.add_middleware(
 async def read_root() -> dict:
     return {"message": "Welcome to our wait management system."}
 
+############ ORDER #################
+
+@app.post("order/cart/add")
+def ordre_cart_add():
+    order.add_order()
+    return {}
+
+@app.get("order/cart/list")
+def ordre_cart_list(table_id: int):
+    return order.get_table_order(table_id)
+
+@app.get("order/listall")
+def ordre_listall():
+    return order.get_all_orders()
+
+############ TABLE #################
 @app.post("/table/select")
 def table_select():
     table.select_table_number()
+    return {}
 
 @app.get("table/status")
 def table_status():
@@ -35,3 +54,4 @@ def table_status():
 @app.post("table/status/update")
 def table_status_update(table_id: int, status: str):
     table.update_table_status(table_id, status)
+    return {}
