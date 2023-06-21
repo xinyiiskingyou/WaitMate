ef5b336254722a08be8a74f306ec5868a9ba7ead
xinyiiskingyou <xinyi_lin217@hotmail.com>
2023-06-21 16:08:06 +1000

    added api methods for tables

--- backend/app/api.py
+++ backend/app/api.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
+from database.table_db import table_DB
 
 app = FastAPI()
+table = table_DB()
 
 origins = [
     "http://localhost:3000",
@@ -22,3 +23,15 @@ app.add_middleware(
 @app.get("/", tags=["root"])
 async def read_root() -> dict:
     return {"message": "Welcome to our wait management system."}
+
+@app.post("/table/select")
+def table_select():
+    table.select_table_number()
+
+@app.get("table/status")
+def table_status():
+    return table.check_all_tables_status()
+
+@app.post("table/status/update")
+def table_status_update(table_id: int, status: str):
+    table.update_table_status(table_id, status)
