81b7556d926667fb550138e183453c6e81c7cea8
xinyiiskingyou <xinyi_lin217@hotmail.com>
2023-06-26 01:21:07 +1000

    updated

--- backend/tests/fixture.py
+++ backend/tests/fixture.py
@@ -1,25 +1,17 @@
 import pytest
-from src.table_db import TableDB
-from src.menu import category_add, item_add
+from src.table import TableDB
+from src.menu import MenuDB
 
+table = TableDB()
 @pytest.fixture
 def table_id_1():
-    table = TableDB()
     table.clear_tables_data()
     return table.select_table_number(1)
     
 @pytest.fixture
 def table_id_2():
-    table = TableDB()
     return table.select_table_number(2)
 
 @pytest.fixture
 def table_id_3():
-    table = TableDB()
     return table.select_table_number(3)
-
-
-
-
-
-
