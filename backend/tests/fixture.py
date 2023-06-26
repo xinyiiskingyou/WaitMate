aa88d4b6b425fbdf78d835aa3d7b51b2c864ab3f
xinyiiskingyou <xinyi_lin217@hotmail.com>
2023-06-26 21:28:56 +1000

    added more tests for order

--- backend/tests/fixture.py
+++ backend/tests/fixture.py
@@ -1,8 +1,11 @@
 import pytest
+import os
 from src.table import TableDB
 from src.menu import MenuDB
 
 table = TableDB()
+menu = MenuDB()
+
 @pytest.fixture
 def table_id_1():
     table.clear_tables_data()
@@ -15,3 +18,12 @@ def table_id_2():
 @pytest.fixture
 def table_id_3():
     return table.select_table_number(3)
+
+@pytest.fixture
+def menu_japanese():
+    if os.path.exists("./src/database/menu.db"):
+        os.remove("./src/database/menu.db")
+
+    menu.category_add('Japanese')
+    menu.item_add('Japanese', 'salmon sushi', 10, '_', '_', False)
+    menu.item_add('Japanese', 'dorayaki', 6, '_', '_', False)
--- backend/tests/order_test.py
+++ backend/tests/order_test.py
@@ -2,7 +2,7 @@ import pytest
 
 from src.order import OrderDB
 from src.error import InputError
-from tests.fixture import table_id_1, table_id_2, table_id_3
+from tests.fixture import table_id_1, table_id_2, table_id_3, menu_japanese
 
 order = OrderDB()
 
@@ -24,7 +24,13 @@ def test_add_order_invalid_amount(table_id_1):
     with pytest.raises(InputError):
         order.add_order(table_id_1, 'sushi', 0)
 
-# TODO: check valid menu item
+def test_add_order_invalid_item_name(table_id_1, menu_japanese):
+    order.clear_order_table()
+
+    with pytest.raises(InputError):
+        order.add_order(table_id_1, 'sushi', 1)
+    with pytest.raises(InputError):
+        order.add_order(table_id_1, 'tuna sushi', 1)
 
 def test_list_order_invalid_table_id():
     order.clear_order_table()
@@ -36,26 +42,26 @@ def test_list_order_invalid_table_id():
     with pytest.raises(InputError):
         order.get_table_order(100)
 
-def test_valid_order_list(table_id_1, table_id_2):
+def test_valid_order_list(table_id_1, table_id_2, menu_japanese):
     order.clear_order_table()
 
-    order.add_order(table_id_1, 'sushi', 1)
+    order.add_order(table_id_1, 'salmon sushi', 1)
     order.add_order(table_id_1, 'dorayaki', 1)
 
     table_order = order.get_table_order(table_id_1)
     assert len(table_order) == 2
 
-    order.add_order(table_id_2, 'sushi', 1)
+    order.add_order(table_id_2, 'salmon sushi', 1)
     table_order = order.get_table_order(table_id_2)
     assert len(table_order) == 1
 
-def test_get_all_orders(table_id_1, table_id_2, table_id_3):
+def test_get_all_orders(table_id_1, table_id_2, table_id_3, menu_japanese):
 
     order.clear_order_table()
 
     order.add_order(table_id_1, 'salmon sushi', 2)
     order.add_order(table_id_2, 'dorayaki', 1)
-    order.add_order(table_id_3, 'coke', 2)
+    order.add_order(table_id_3, 'dorayaki', 2)
 
     table_order = order.get_all_orders()
 
