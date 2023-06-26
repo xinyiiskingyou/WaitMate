bbb62f3f1c3ecab9ce0d17e083062bbe84a7af0d
Trinh <trinh.pham2277@gmail.com>
2023-06-26 20:48:41 +1000

    Changes to cart frontend for testing

--- frontend/src/components/Cart.jsx
+++ frontend/src/components/Cart.jsx
@@ -19,6 +19,9 @@ function createData(name, amount) {
 const rows = [
   createData('Frozen yoghurt', 1),
   createData('Ice cream sandwich', 2),
+  createData('Eclair', 1),
+  createData('Cupcake', 1),
+
 ];
 
 const Cart = () => {
@@ -29,29 +32,6 @@ const Cart = () => {
     console.log('hello')
   };
   
-  const handleAddAmount = (itemname) => {
-    rows.forEach(function(element, index, array){
-        if (element.name === itemname) {
-          array[index].amount++;
-          setAmount(amount+1);
-          console.log(array[index]);
-        }
-    });
-  };
-
-  const handleSubAmount = (itemname) => {
-    rows.forEach(function(element, index, array){
-      if (element.name === itemname) {
-        if (array[index].amount === 1) {
-          return;
-        }
-        array[index].amount--;
-        setAmount(amount-1);
-        console.log(array[index]);
-      }
-    });
-  };
-
   let getCart = async () => {
     let response = await fetch('http://localhost:8000/order/cart/0')
     let data = await response.json()
@@ -64,8 +44,8 @@ const Cart = () => {
       <Box
         sx={{ 
           margin: 2, 
+          mt: 4, 
           borderRadius: 2, 
-          bgcolor: '#ECEBEB',
           height: '100%',
           display:"flex",
           flexDirection:"column"
@@ -74,7 +54,13 @@ const Cart = () => {
         <Grid container spacing={2}>
           <Grid item xs={2}>
             <Link to="/">
-              <Button variant="outlined" color="primary" >
+              <Button              
+                sx={{ 
+                  border: 5,
+                  borderColor: '#F5BBCC',
+                  borderRadius: 2,
+                  color: 'black' 
+                }}>
                 <Typography variant="h4">Back</Typography>
               </Button>
             </Link>
@@ -99,49 +85,34 @@ const Cart = () => {
       <Box
         sx={{ 
           margin: 2, 
-          border: 5,
+          border: 10,
           borderColor: '#F5BBCC',
           borderRadius: 2, 
           display:"flex",
         }}>
         <Grid container direction="column">
           <Grid item>
-            <TableContainer>
-              <Table aria-label='custom pagination table'>
+            <TableContainer  sx={{
+                height: 500,
+                pt: 4,  
+              }}>
+              <Table aria-label='custom pagination table' >
                 <TableBody>
                   {rows.map((row) => (
                     <TableRow key={row.name}>
-                      <TableCell component='th' scope='row' sx={{ fontSize: 20, borderBottom: 'none'}}>
+                      <TableCell component='th' scope='row' sx={{ fontSize: 30, borderBottom: 'none', pl: 10}}>
                         {row.name}
                       </TableCell>
-                      <TableCell style={{ width: 160 }} align='right' sx={{borderBottom: 'none' }}>
-                        <Button onClick={() => handleSubAmount(row.name)}>-</Button>
-                          {row.amount}
-                        <Button onClick={() => handleAddAmount(row.name)}>+</Button>
+                      <TableCell style={{ width: 160 }} align='right' sx={{ fontSize: 25, borderBottom: 'none', pr: 10}}>
+                        {row.amount}
                       </TableCell>
                     </TableRow>
                   ))}
-                  {emptyRows > 0 && (
-                    <TableRow style={{ height: 90 * emptyRows}} >
-                      <TableCell colSpan={1} sx={{ borderBottom: 'none' }}/>
-                    </TableRow>
-                  )}
                 </TableBody>
               </Table>
             </TableContainer>
           </Grid>
 
-          <Grid item align='right'>
-            <Button onClick={() => printhello()}       
-              sx={{ 
-              margin: 2, 
-              border: 5,
-              borderColor: '#F5BBCC',
-              borderRadius: 2, 
-              }}>
-              Order
-            </Button>
-          </Grid>
         </Grid>
       </Box>
       </Grid>
