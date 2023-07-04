85ac12bf558cc71a8bc8d7d943f4667bfdc099c5
Trinh <trinh.pham2277@gmail.com>
2023-07-04 23:26:38 +1000

    Adding extra endpoint for checkout and adding lines for call to checkout func

--- backend/app/api.py
+++ backend/app/api.py
@@ -6,6 +6,7 @@ from src.table import TableDB
 from src.menu import MenuDB
 from src.notifications import Notifications
 from src.tracking import Tracking
+from src.checkout import Checkout
 from src.model.category_req import Category
 from src.model.category_order_req import CategoryOrder
 from src.model.category_update_req import CategoryUpdate
@@ -24,6 +25,7 @@ table = TableDB()
 menu = MenuDB()
 track = Tracking()
 notification = Notifications()
+checkout = Checkout()
 
 origins = [
     'http://localhost:3000',
@@ -152,18 +154,25 @@ def waitstaff_get_from_customer():
 
 ############ CHECKOUT #################
 
+@app.get('/checkout/order/{id}')
+def checkout_order_api(id: int):
+    return checkout.checkout_order(id)
+
 @app.get('/checkout/bill/{id}')
 def checkout_bill_api(id: int):
-    return {}
+    return checkout.checkout_bill(id)
 
 @app.post('/checkout/bill/tips')
 def checkout_bill_tips_api(reqbody: Tip):
+    checkout.checkout_bill_tips(reqbody.id, reqbody.amount)
     return {}
 
 @app.post('/checkout/bill/coupon')
 def checkout_bill_coupon_api(reqbody: Coupon_Cust):
+    checkout.checkout_bill_coupon(reqbody.id, reqbody.code)
     return {}
 
 @app.post('/checkout/coupon/create')
 def checkout_coupon_create_api(reqbody: Coupon):
+    checkout.checkout_coupon_create(reqbody.code, reqbody.int)
     return {}
