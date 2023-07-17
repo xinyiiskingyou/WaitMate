55f636d71e56fdfb159a4cec5e486517c67be8ed
Trinh <trinh.pham2277@gmail.com>
2023-07-17 21:20:02 +1000

    Adding a todo

--- backend/src/auth.py
+++ backend/src/auth.py
@@ -1,10 +1,11 @@
-# import pyrebase
-import json
-import firebase
-import smtplib, ssl
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
+'''
+The `auth` module provides functionalites for authentication and authorisation related features.
 
+These features includes login and tokenised access, as well as user
+management for the manager
+'''
+import firebase
+import re   
 from firebase_admin import auth as fb_auth, credentials, initialize_app
 from src.error import InputError, AccessError
 
@@ -16,6 +17,7 @@ class Auth:
     KITCHENSTAFF_EMAIL = 'kitchenstaff@waitmate.com'
     MANAGER_EMAIL= 'manager@waitmate.com'
     PASSWORD = 'waitmate1'
+    EMAIL_CHECK = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'  
 
     def __init__(self):
         cred = credentials.Certificate("./src/waitmate-ba463-firebase-adminsdk-ea5im-72b77a7f6f.json")
@@ -62,11 +64,12 @@ class Auth:
         print('Sucessfully created kitchenstaff: {0}'.format(user.uid))
 
     def create_customer(self):
+        #TODO
         self.auth.sign_in_anonymous()
 
     def login_mananger(self, email: str, password: str):
         if not email or not password:
-            raise AccessError('Enter your email and password')
+            raise InputError('Enter your email and password')
         try:
             user = self.auth.sign_in_with_email_and_password(email, password)            
             print('Successfully logged in: manager')    
@@ -75,12 +78,9 @@ class Auth:
         except:
             raise AccessError('Invalid email or password')
         
-    def logout(self, token):
-        pass
-
     def login_staff(self, password: str, is_waitstaff: bool):
         if not password:
-            raise AccessError('Enter your password')
+            raise InputError('Enter your password')
         
         email: str = WAITSTAFF_EMAIL if is_waitstaff else KITCHENSTAFF_EMAIL
         print(email)
@@ -93,15 +93,12 @@ class Auth:
             raise AccessError('Invalid password')
 
     def is_authenticated(self, token):
-        print(token)
-
         try:
             user = fb_auth.verify_id_token(token)
             print('User is authenticated')
             return user
         except:
             raise AccessError('Authentication failed')
-
     
     def is_authorized(self, roles: list, user: dict):
         print(user)
@@ -114,14 +111,12 @@ class Auth:
         self.auth.send_password_reset_email(user['user']['email'])
         print('Sucessfully updated manager password')
 
-    def change_email_mananger(self, user: dict, token: str, new_email: str):
-        if len(new_email) == 0:
-            raise InputError('Invalid email size')
+    def change_email_mananger(self, user: dict, new_email: str):
+        if not re.search(self.EMAIL_CHECK, new_email):
+            raise InputError('Invalid email')
 
         print(user['uid'], new_email)
-        _ = fb_auth.update_user(
-            user['uid'],
-            email=new_email)
+        fb_auth.update_user( user['uid'], email=new_email)
         
         print('Sucessfully updated manager email')
 
@@ -130,6 +125,7 @@ class Auth:
             raise InputError('Invalid password size')
         
         email: str = WAITSTAFF_EMAIL if is_waitstaff else KITCHENSTAFF_EMAIL
+
         print(fb_auth.get_user_by_email(email).uid)
         fb_auth.update_user(
             fb_auth.get_user_by_email(email).uid,
@@ -138,13 +134,7 @@ class Auth:
 
     def delete_all(self):
         for user in fb_auth.list_users().iterate_all():
-            result = fb_auth.delete_user(user.uid)
-
-            # if result.errors:
-            #     return 
-            # for _ in result.errors:
-            #     print('error #{0}, reason: {1}'.format(result.index, result.reason))
-
+            fb_auth.delete_user(user.uid)
         self.create_restaurant()
 
     def print_all(self):
