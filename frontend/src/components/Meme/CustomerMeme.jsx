948f5728cb4ee72a7bf58112acbd62e9e8f8d522
Sunnie Leung <sunnie@Sunnies-MBP.modem>
2023-07-30 21:55:11 +1000

    progress update

--- frontend/src/components/Meme/CustomerMeme.jsx
+++ frontend/src/components/Meme/CustomerMeme.jsx
@@ -19,10 +19,6 @@ import {
   PaginationItem,
 } from "@mui/material";
 import { Link, useParams } from 'react-router-dom';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import ListCategories from './Category/ListCategories';
-import ListItems from './Items/ListItems';
-import BrowseItems from './Items/BrowseItems';
 import SendNotification from "../Notifications/SendNotification";
 import WaitMate from "../../assets/WaitMate.png";
 
@@ -59,7 +55,7 @@ const buttonStyle = {
 const ITEMS_PER_PAGE = 6;
 
 const CustomerMeme = () => {
-  
+  const id = useParams();
   return (
     <Container maxWidth="sm">
       <ThemeProvider theme={theme}>
@@ -85,111 +81,6 @@ const CustomerMeme = () => {
           </AppBar>
         </div>
       </ThemeProvider>
-
-      <div style={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        height: '100vh',
-      }}>
-        <Drawer
-          variant="permanent"
-          PaperProps={{ style: { 
-            marginTop: "90px",
-            width: '250px', 
-            height: "86vh", 
-            borderTopRightRadius: '20px',
-            borderBottomRightRadius: '20px'
-          }}}>
-          <div style={{ display: "flex", alignContent: "center", justifyContent: "center" }}>
-            <Typography variant='h5'style={{margin: "20px"}}>
-              Menu Category
-            </Typography>
-          </div>
-
-          { Object.entries(categories).map(([index, category]) => (
-            <List key={category}>
-              <ListItem disablePadding value={category} onClick={()=>handleCategoryChange(index, category)}>
-                <ListItemButton>
-                  <ListItemText 
-                    primary={category.toUpperCase()}
-                    primaryTypographyProps={{ 
-                      style: { 
-                        fontSize: '1vw', 
-                        border: category === currentCategory ? "5px solid #FFA0A0" :"5px solid #bdbdbd",
-                        borderRadius: 18, 
-                        padding: '0.5vh',
-                        textAlign: "center",
-                        background: category === currentCategory ? "#FFCFCF" : "#E0E0E0",
-                        marginBottom: '-1.5vh'
-                      } 
-                    }} 
-                  />
-                </ListItemButton>
-              </ListItem>
-            </List>
-          ))}
-
-          <Grid container direction="column" spacing={0}>
-            <Link to={cartLink}>
-              <IconButton sx={{
-                margin: '15%', 
-                spacing: '-20', 
-                width: '70%', 
-                height: '5vh',
-                border: "6px solid #FFA0A0",
-                background: "#FFCFCF",
-                color: 'black',
-                fontSize: '15px',
-                borderRadius: 8,
-              }}>
-                Order Summary <ShoppingCartIcon />
-              </IconButton>
-            </Link>
-          </Grid>
-        </Drawer>
-        
-        {categoryID !== -1 ? (
-          <Paper elevation={3} sx={{
-            padding: "20px",
-            borderRadius: "8px",
-            width: "1150px", 
-            height: "570px", 
-            marginLeft: "200px",
-            position: 'fixed',
-            marginTop: '60px',
-          }}>
-            <div style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', gap: '1vw'}}>
-              {Object.entries(itemsForCurrentPage)
-                .filter(([_, menuItem]) => menuItem.name !== null)
-                .map(([index, menuItem]) => (
-                  <BrowseItems
-                    itemName={menuItem.name}
-                    itemPrice={menuItem.cost}
-                    itemDescription={menuItem.description}
-                    itemIngredient={menuItem.ingredients} 
-                    itemVegetarian={menuItem.is_vegan}
-                    tableID={id.id}
-                  />
-                ))}
-            </div>
-          </Paper>
-        ) : (<></>)}
-
-        <div style={{ position: 'fixed', bottom: '22px', left: '55%', transform: 'translateX(-50%)' }}>
-          <Pagination
-            count={Math.ceil(menuItems.length / ITEMS_PER_PAGE)}
-            page={currentPage}
-            onChange={handlePageChange}
-            renderItem={(item) => (
-              <PaginationItem
-                component={IconButton}
-                {...item}
-              />
-            )}
-          />
-        </div>
-      </div>
     </Container>
   );
 };
