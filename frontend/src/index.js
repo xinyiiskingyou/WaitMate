c8a788a4a14250c816d41e5f6ac54031e3f0b08f
Trinh <trinh.pham2277@gmail.com>
2023-07-31 22:17:16 +1000

    Adding changesfrom rebase

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -12,20 +12,12 @@ import ManageMenu from './components/Menu/ManageMenu';
 import ManageMeme from './components/Meme/ManageMeme';
 import SettingPage from './components/LoginPage/ManagerSettingsPage';
 import Coupon from './components/Coupon/CouponPage';
-<<<<<<< HEAD
 import KitchenstaffLogin from './components/LoginPage/KitchenstaffLogin'
 import KitchenInterface from './components/UserInterface/KitchenInterface';
 import WaitstaffLogin from './components/LoginPage/WaitstaffLogin';
 import WaitStaffInterface from './components/UserInterface/WaitStaffInterface';
 import { getToken } from './auth.js';
 import GamePage from './components/Activity/ActivityGamePage';
-=======
-import KitchenstaffLogin from './components/Staff/KitchenstaffLogin'
-import KitchenInterface from './components/Staff/KitchenInterface';
-import WaitstaffLogin from './components/Staff/WaitstaffLogin';
-import WaitStaffInterface from './components/Staff/WaitStaffInterface';
-import { getToken } from './auth.js';
->>>>>>> 0f929a4f4 (merged with s3sunnie)
 import CustomerTooBored from './components/Activity/CustomerTooBored';
 import CustomerMeme from './components/Meme/CustomerMeme';
 
@@ -63,11 +55,8 @@ function App() {
         <Route path="/customer/browse/:id" element={<BrowseMenu />} />
         <Route path="/customer/cart/:id" element={<ViewCart />} />
         <Route path="/customer/activity/:id" element={<CustomerTooBored />} />
-<<<<<<< HEAD
         <Route path="/customer/activity/gamepage/:id" element={<GamePage />} />
 
-=======
->>>>>>> 83281f792 (add path for meme)
         <Route path="/customer/meme/:id" element={<CustomerMeme />} />
         <Route path="/staff" element={<Staff />} />
         <Route path="/manager/login" element={<ManagerLogin />} />
@@ -79,13 +68,13 @@ function App() {
         <Route path="/waitstaff/list" element={<WaitStaffInterface />} />
         <Route path="/kitchenstaff/login" element={<KitchenstaffLogin />} />
         <Route path="/kitchenstaff/list" element={<KitchenInterface />} />
-<<<<<<< HEAD
         <Route path="/customer/activity/:id" element={<CustomerTooBored />} />
         <Route path="/customer/meme/:id" element={<CustomerMeme />} />
-=======
+
         <Route path="/toobored/:id" element={<CustomerTooBored />} />
         <Route path="/customermeme/:id" element={<CustomerMeme />} />
->>>>>>> 0f929a4f4 (merged with s3sunnie)
+
+
       </Routes>
     </Router>
   );
